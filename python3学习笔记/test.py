f75bceadd7574d36f3a3698038e9b593ed59f08d
wanli <1301281985@qq.com>
2018-01-19 18:12:16 +0800

    Site updated: 2018-01-19 18:12:15

--- python3学习笔记/test.py
+++ python3学习笔记/test.py
@@ -1,1 +1,21 @@
-print('this is test file')
+def func():
+    spam = "func_scope"
+    print(locals())
+    
+    def do_local():
+        spam = "local spam"
+        print('do_local_scope:', locals())
+    def do_nonlocal():
+        nonlocal spam  # 浅拷贝func中的locals['spam']，并创建spam标识符
+        print('do_nonlocal_scope:', locals())
+        spam = "nonlocal spam"
+    def do_global():
+        global spam
+        print('do_global_scope:', locals())
+        spam = "global spam"
+        
+    do_local()
+    do_nonlocal()
+    do_global()
+
+func()
